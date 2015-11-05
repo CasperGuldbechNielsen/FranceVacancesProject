5fcd8aef93eb9ed3500de870e7e95549572648f3
CasperGuldbechNielsen <casp674d@edu.easj.dk>
2015-11-05 12:56:22 +0100

    Added pinpoints (not working yet)

--- Hamburger Heaven Challenge/Hamburger Heaven Challenge/Map.xaml.cs
+++ Hamburger Heaven Challenge/Hamburger Heaven Challenge/Map.xaml.cs
@@ -1,6 +1,6 @@
-﻿using Windows.Devices.Geolocation;
+﻿using System.Collections.ObjectModel;
+using Windows.Devices.Geolocation;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Navigation;
 
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
@@ -12,12 +12,18 @@ namespace Hamburger_Heaven_Challenge
     /// </summary>
     public sealed partial class Map : Page
     {
+        private ObservableCollection<PushPin> items;
 
         public Map()
         {
             this.InitializeComponent();
             MyMap.Center = new Geopoint(new BasicGeoposition() {Latitude = 46.8442643, Longitude = 2.5992004 });
-        }
+            MyMap.ZoomLevel = 6;
 
+            MapItems.ItemsSource = items;
+
+            items = new ObservableCollection<PushPin>();
+            items.Add(new PushPin(48.871905, 2.343217));
+        }
     }
 }
---
+++ Hamburger Heaven Challenge/Hamburger Heaven Challenge/PushPin.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Windows.Devices.Geolocation;
+
+namespace Hamburger_Heaven_Challenge
+{
+    
+    internal class PushPin
+    {
+
+        public Geopoint Geopoint { get; set; }
+
+
+        public PushPin(double latitude, double longitude)
+        {
+            Geopoint = new Geopoint(new BasicGeoposition() { Latitude = latitude, Longitude = longitude });
+        }
+    }
+}
