30f4b8648254937ec20c507734367b19503f07f9
ystvan <istv0050@edu.easj.dk>
2015-11-09 08:47:06 +0100

    most popular buttons

--- Hamburger Heaven Challenge/Hamburger Heaven Challenge/MostPopular.xaml.cs
+++ Hamburger Heaven Challenge/Hamburger Heaven Challenge/MostPopular.xaml.cs
@@ -26,5 +26,83 @@ namespace Hamburger_Heaven_Challenge
         {
             this.InitializeComponent();
         }
+
+
+        // User refine search by comboBox as follows:
+
+        //private void ComboBox_RegionSelectionChanged(object sender, SelectionChangedEventArgs e)
+        //{
+        //    if (RegionResultTextBlock == null) return;
+
+        //    var combo = (ComboBox)sender;
+        //    var item = (ComboBoxItem)combo.SelectedItem;
+        //    RegionResultTextBlock.Text = item.Content.ToString();
+        //}
+
+        //private void ComboBox_SortBySelectionChanged(object sender, SelectionChangedEventArgs e)
+        //{
+        //    if (SortResultTextBlock == null) return;
+
+        //    var combo = (ComboBox)sender;
+        //    var item = (ComboBoxItem)combo.SelectedItem;
+        //    SortResultTextBlock.Text = item.Content.ToString();
+        //}
+
+        //private void ComboBox_RoomsSelectionChanged(object sender, SelectionChangedEventArgs e)
+        //{
+        //    if (RoomsResultTextBlock == null) return;
+
+        //    var combo = (ComboBox)sender;
+        //    var item = (ComboBoxItem)combo.SelectedItem;
+        //    RoomsResultTextBlock.Text = item.Content.ToString();
+        //}
+
+
+
+        // User refine the search by flyout buttons as follows:    
+
+        private string _region;
+        private string _roomNumber;
+        private string _sortBy;
+
+
+        private void Region_Click(object sender, RoutedEventArgs e)
+        {
+            var selected = (MenuFlyoutItem)sender;
+            _region = selected.Text;
+            displayResult();
+        }
+
+        private void SortBy_Click(object sender, RoutedEventArgs e)
+        {
+            var selected = (MenuFlyoutItem)sender;
+            _sortBy = selected.Text;
+            displayResult();
+        }
+
+        private void RoomNumber_Click(object sender, RoutedEventArgs e)
+        {
+            var selected = (MenuFlyoutItem)sender;
+            _roomNumber = selected.Text;
+            displayResult();
+        }
+        private void displayResult()
+        {
+            if (_region == "None" || String.IsNullOrEmpty(_region))
+            {
+                ResultTextBlock.Text = "Please select a region";
+                return;
+            }
+
+            ResultTextBlock.Text = _region;
+
+            if (_sortBy != "None" && !String.IsNullOrEmpty(_sortBy))
+                ResultTextBlock.Text += " + " + _sortBy;
+
+            if (_roomNumber != "None" && !String.IsNullOrEmpty(_roomNumber))
+                ResultTextBlock.Text += " + " + _roomNumber;
+
+
+        }
     }
 }
