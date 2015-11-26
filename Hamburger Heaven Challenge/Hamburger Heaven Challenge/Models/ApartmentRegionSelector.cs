5d4a85bd9e81a9eb0bde5b0669c1851be3fe6511
ystvan <istv0050@edu.easj.dk>
2015-11-26 22:16:35 +0100

    new classes to select apartments

---
+++ Hamburger Heaven Challenge/Hamburger Heaven Challenge/Models/ApartmentRegionSelector.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hamburger_Heaven_Challenge.Models
+{
+    class ApartmentRegionSelector
+    {
+        public RegionCategory ApartmentRegion { get; set; }
+    }
+}
--- Hamburger Heaven Challenge/Hamburger Heaven Challenge/MostPopular.xaml.cs
+++ Hamburger Heaven Challenge/Hamburger Heaven Challenge/MostPopular.xaml.cs
@@ -28,69 +28,88 @@ namespace Hamburger_Heaven_Challenge
 
         private ObservableCollection<Apartment> Apartments;
 
+        private List<ApartmentRegionSelector> ApartmentRegionSelector;
+        private List<ApartmentCitySelector> ApartmentCitySelector;
+        private List<ApartmentRoomSelector> ApartmentRoomSelector; 
+
         
-            public MostPopular()
-            {
-                this.InitializeComponent();
-                Apartments = new ObservableCollection<Apartment>();
-                ApartmentManager.GetAllApartments(Apartments);
-            }
+        public MostPopular()
+        {
+          this.InitializeComponent();
+          Apartments = new ObservableCollection<Apartment>();
+          ApartmentManager.GetAllApartments(Apartments);
+
+            ApartmentRegionSelector.Add(new ApartmentRegionSelector { ApartmentRegion = RegionCategory.Alsace});
+            ApartmentRegionSelector.Add(new ApartmentRegionSelector { ApartmentRegion = RegionCategory.Aquitaine });
+            ApartmentRegionSelector.Add(new ApartmentRegionSelector { ApartmentRegion = RegionCategory.Auvergne });
+            ApartmentRegionSelector.Add(new ApartmentRegionSelector { ApartmentRegion = RegionCategory.BasseNormandie });
+            ApartmentRegionSelector.Add(new ApartmentRegionSelector { ApartmentRegion = RegionCategory.Bretagne });
+            ApartmentRegionSelector.Add(new ApartmentRegionSelector { ApartmentRegion = RegionCategory.Bourgogne });
+            ApartmentRegionSelector.Add(new ApartmentRegionSelector { ApartmentRegion = RegionCategory.CentreValDeLoire });
+            ApartmentRegionSelector.Add(new ApartmentRegionSelector { ApartmentRegion = RegionCategory.ChampagneArdenne });
+            ApartmentRegionSelector.Add(new ApartmentRegionSelector { ApartmentRegion = RegionCategory.Corsica });
+            ApartmentRegionSelector.Add(new ApartmentRegionSelector { ApartmentRegion = RegionCategory.FrancheComté });
+            ApartmentRegionSelector.Add(new ApartmentRegionSelector { ApartmentRegion = RegionCategory.HauteNormandie });
+            ApartmentRegionSelector.Add(new ApartmentRegionSelector { ApartmentRegion = RegionCategory.ÎleDeFrance });
+            ApartmentRegionSelector.Add(new ApartmentRegionSelector { ApartmentRegion = RegionCategory.LanguedocRoussillon });
+            ApartmentRegionSelector.Add(new ApartmentRegionSelector { ApartmentRegion = RegionCategory.Limousin });
+            ApartmentRegionSelector.Add(new ApartmentRegionSelector { ApartmentRegion = RegionCategory.Lorraine });
+            ApartmentRegionSelector.Add(new ApartmentRegionSelector { ApartmentRegion = RegionCategory.MidiPyrénées });
+            ApartmentRegionSelector.Add(new ApartmentRegionSelector { ApartmentRegion = RegionCategory.NordPasDeCalais });
+            ApartmentRegionSelector.Add(new ApartmentRegionSelector { ApartmentRegion = RegionCategory.PaysDeLaLoire });
+            ApartmentRegionSelector.Add(new ApartmentRegionSelector { ApartmentRegion = RegionCategory.Picardie });
+            ApartmentRegionSelector.Add(new ApartmentRegionSelector { ApartmentRegion = RegionCategory.PoitouCharentes });
+            ApartmentRegionSelector.Add(new ApartmentRegionSelector { ApartmentRegion = RegionCategory.ProvenceAlpesCôteDAzur });
+            ApartmentRegionSelector.Add(new ApartmentRegionSelector { ApartmentRegion = RegionCategory.RhôneAlpes });
+
+
+            ApartmentCitySelector.Add(new ApartmentCitySelector{ ApartmentCity = CityCategory.Ajaccio });
+            ApartmentCitySelector.Add(new ApartmentCitySelector { ApartmentCity = CityCategory.Strasbourg });
+            ApartmentCitySelector.Add(new ApartmentCitySelector { ApartmentCity = CityCategory.Bordeaux });
+            ApartmentCitySelector.Add(new ApartmentCitySelector { ApartmentCity = CityCategory.ClermontFerrand });
+            ApartmentCitySelector.Add(new ApartmentCitySelector { ApartmentCity = CityCategory.Rennes });
+            ApartmentCitySelector.Add(new ApartmentCitySelector { ApartmentCity = CityCategory.Dijon });
+            ApartmentCitySelector.Add(new ApartmentCitySelector { ApartmentCity = CityCategory.Orléans });
+            ApartmentCitySelector.Add(new ApartmentCitySelector { ApartmentCity = CityCategory.ChâlonsEnChampagne });
+            ApartmentCitySelector.Add(new ApartmentCitySelector { ApartmentCity = CityCategory.Paris });
+            ApartmentCitySelector.Add(new ApartmentCitySelector { ApartmentCity = CityCategory.Montpellier });
+            ApartmentCitySelector.Add(new ApartmentCitySelector { ApartmentCity = CityCategory.Limoges });
+            ApartmentCitySelector.Add(new ApartmentCitySelector { ApartmentCity = CityCategory.Metz });
+            ApartmentCitySelector.Add(new ApartmentCitySelector { ApartmentCity = CityCategory.Caen });
+            ApartmentCitySelector.Add(new ApartmentCitySelector { ApartmentCity = CityCategory.Tolouse });
+            ApartmentCitySelector.Add(new ApartmentCitySelector { ApartmentCity = CityCategory.Lille });
+            ApartmentCitySelector.Add(new ApartmentCitySelector { ApartmentCity = CityCategory.Nantes });
+            ApartmentCitySelector.Add(new ApartmentCitySelector { ApartmentCity = CityCategory.Amiens });
+            ApartmentCitySelector.Add(new ApartmentCitySelector { ApartmentCity = CityCategory.Poitiers });
+            ApartmentCitySelector.Add(new ApartmentCitySelector { ApartmentCity = CityCategory.Marseille });
+            ApartmentCitySelector.Add(new ApartmentCitySelector { ApartmentCity = CityCategory.Lyon });
+            ApartmentCitySelector.Add(new ApartmentCitySelector { ApartmentCity = CityCategory.Rouen });
+            
+            ApartmentRoomSelector.Add(new ApartmentRoomSelector { ApartmentRoomNumber = RoomCategory.One});
+            ApartmentRoomSelector.Add(new ApartmentRoomSelector { ApartmentRoomNumber = RoomCategory.Two });
+            ApartmentRoomSelector.Add(new ApartmentRoomSelector { ApartmentRoomNumber = RoomCategory.Three });
+            ApartmentRoomSelector.Add(new ApartmentRoomSelector { ApartmentRoomNumber = RoomCategory.Four });
+            ApartmentRoomSelector.Add(new ApartmentRoomSelector { ApartmentRoomNumber = RoomCategory.Five });
+            ApartmentRoomSelector.Add(new ApartmentRoomSelector { ApartmentRoomNumber = RoomCategory.Six });
+            ApartmentRoomSelector.Add(new ApartmentRoomSelector { ApartmentRoomNumber = RoomCategory.Seven });
+            ApartmentRoomSelector.Add(new ApartmentRoomSelector { ApartmentRoomNumber = RoomCategory.Eight });
+
+
+
+        }
 
 
 
         private void GridView_ItemClick(object sender, ItemClickEventArgs e)
         {
-            var apartment = (Apartment)e.ClickedItem;
-            ApartmentResultTextBlock.Text = "You have selected apartment # " + apartment.ApartmentId;
+            var apartments = (Apartment)e.ClickedItem;
+            ApartmentResultTextBlock.Text = "You have selected apartment # " + apartments.ApartmentId;
         }
 
 
+      
 
-        // User refine the search by flyout buttons as follows:::
-
-        private string _region;
-        private string _roomNumber;
-        private string _sortBy;
-
-
-        private void Region_Click(object sender, RoutedEventArgs e)
-        {
-            var selected = (MenuFlyoutItem)sender;
-            _region = selected.Text;
-            DisplayResult();
-        }
-
-        private void SortBy_Click(object sender, RoutedEventArgs e)
-        {
-            var selected = (MenuFlyoutItem)sender;
-            _sortBy = selected.Text;
-            DisplayResult();
-        }
-
-        private void RoomNumber_Click(object sender, RoutedEventArgs e)
-        {
-            var selected = (MenuFlyoutItem)sender;
-            _roomNumber = selected.Text;
-            DisplayResult();
-        }
-        private void DisplayResult()
-        {
-            if (_region == "None" || String.IsNullOrEmpty(_region))
-            {
-                ResultTextBlock.Text = "Please select a region";
-                return;
-            }
-
-            ResultTextBlock.Text = _region;
-
-            if (_sortBy != "None" && !String.IsNullOrEmpty(_sortBy))
-                ResultTextBlock.Text += " + " + _sortBy;
-
-            if (_roomNumber != "None" && !String.IsNullOrEmpty(_roomNumber))
-                ResultTextBlock.Text += " + " + _roomNumber;
-
-
-        }
+        
     }
 }
 
