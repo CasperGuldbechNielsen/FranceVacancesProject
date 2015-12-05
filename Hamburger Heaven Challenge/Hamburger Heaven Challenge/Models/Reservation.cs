9bdecb079227cdcb47a48d49139ad3d9fc0d72c2
ystvan <istv0050@edu.easj.dk>
2015-12-05 12:27:23 +0100

    new classes to make booking

---
+++ Hamburger Heaven Challenge/Hamburger Heaven Challenge/Models/Reservation.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hamburger_Heaven_Challenge.Models
+{
+    class Reservation
+    {
+       
+        private DateTime _arrival;
+        private DateTime _departure;
+        private TimeSpan _staying;
+
+        public DateTime Arrival
+        {
+            get { return _arrival; }
+            set
+            {
+                // Throwing nice and fancy exceptions we can use in Unitesting
+                _arrival = value;
+            }
+        }
+
+        public DateTime Departure
+        {
+            get { return _departure; }
+            set
+            {
+                // Throwing nice and fancy exceptions we can use in Unitesting
+                _departure = value;
+            }
+        }
+
+        public TimeSpan Staying
+        {
+            get { return _staying; }
+            set
+            {
+                // Throwing nice and fancy exceptions we can use in Unitesting
+                _staying = value;
+            }
+        }
+    }
+}
---
+++ Hamburger Heaven Challenge/Hamburger Heaven Challenge/Models/ReservationManager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hamburger_Heaven_Challenge.Models
+{
+    public static class ReservationManager
+    {
+        public static void MakeReservation(DateTime arrivalDate, DateTime departureDate, TimeSpan lenghtStaying )
+        {
+            // bool isAvailable
+        }
+    }
+}
