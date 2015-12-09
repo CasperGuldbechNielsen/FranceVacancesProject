238e49688984499f4012b8ae8a6ec25b9afa5e4f
ystvan <istv0050@edu.easj.dk>
2015-12-09 13:54:54 +0100

    UNIT TEST SUCCESS

--- Hamburger Heaven Challenge/UnitTestFranceVacances/UnitTest.cs
+++ Hamburger Heaven Challenge/UnitTestFranceVacances/UnitTest.cs
@@ -48,133 +48,313 @@ namespace UnitTestFranceVacances
         [TestMethod]
         public void ApartmentRegionIsNull()
         {
-            
+            Exception caughtException = null;
+            try
+            {
+                TestApartment.ApartmentId = null;
+            }
+            catch (Exception exceptionThrown)
+            {
+                caughtException = exceptionThrown;
+            }
+            Assert.IsNotNull(caughtException);
         }
 
         [TestMethod]
 
         public void ApartmentRegionIsEmpty()
         {
-            
+            Exception caughtException = null;
+            try
+            {
+                TestApartment.ApartmentId = "";
+            }
+            catch (Exception exceptionThrown)
+            {
+                caughtException = exceptionThrown;
+            }
+            Assert.IsNotNull(caughtException);
         }
 
         [TestMethod]
 
         public void ApartmentCityIsNull()
         {
-            
+            Exception caughtException = null;
+            try
+            {
+                TestApartment.ApartmentId = null;
+            }
+            catch (Exception exceptionThrown)
+            {
+                caughtException = exceptionThrown;
+            }
+            Assert.IsNotNull(caughtException);
         }
 
         [TestMethod]
 
         public void ApartmentCityIsEmpty()
         {
-            
+            Exception caughtException = null;
+            try
+            {
+                TestApartment.ApartmentId = "";
+            }
+            catch (Exception exceptionThrown)
+            {
+                caughtException = exceptionThrown;
+            }
+            Assert.IsNotNull(caughtException);
         }
 
         [TestMethod]
 
         public void ApartmentRoomNumberIsNull()
         {
-            
+            Exception caughtException = null;
+            try
+            {
+                TestApartment.ApartmentId = null;
+            }
+            catch (Exception exceptionThrown)
+            {
+                caughtException = exceptionThrown;
+            }
+            Assert.IsNotNull(caughtException);
         }
 
         [TestMethod]
 
         public void ApartmentRoomNumberIsEmpty()
         {
-            
+            Exception caughtException = null;
+            try
+            {
+                TestApartment.ApartmentId = "";
+            }
+            catch (Exception exceptionThrown)
+            {
+                caughtException = exceptionThrown;
+            }
+            Assert.IsNotNull(caughtException);
         }
 
         [TestMethod]
 
         public void ApartmentCoverImageIsNull()
         {
-            
+            Exception caughtException = null;
+            try
+            {
+                TestApartment.ApartmentCoverImage = null;
+            }
+            catch (Exception exceptionThrown)
+            {
+                caughtException = exceptionThrown;
+            }
+            Assert.IsNotNull(caughtException);
         }
 
         [TestMethod]
 
         public void ApartmentCoverImageIsEmpty()
         {
-            
+            Exception caughtException = null;
+            try
+            {
+                TestApartment.ApartmentId = "";
+            }
+            catch (Exception exceptionThrown)
+            {
+                caughtException = exceptionThrown;
+            }
+            Assert.IsNotNull(caughtException);
         }
 
         [TestMethod]
 
         public void ApartmentInsideImageIsNull()
         {
-            
+            Exception caughtException = null;
+            try
+            {
+                TestApartment.ApartmentInsideImage = null;
+            }
+            catch (Exception exceptionThrown)
+            {
+                caughtException = exceptionThrown;
+            }
+            Assert.IsNotNull(caughtException);
         }
 
         [TestMethod]
 
         public void ApartmentInsideImageIsEmpty()
         {
-            
+            Exception caughtException = null;
+            try
+            {
+                TestApartment.ApartmentId = "";
+            }
+            catch (Exception exceptionThrown)
+            {
+                caughtException = exceptionThrown;
+            }
+            Assert.IsNotNull(caughtException);
         }
 
         [TestMethod]
 
         public void ApartmentRatingIsNull()
         {
-            
+            Exception caughtException = null;
+            try
+            {
+                TestApartment.ApartmentCoverImage = null;
+            }
+            catch (Exception exceptionThrown)
+            {
+                caughtException = exceptionThrown;
+            }
+            Assert.IsNotNull(caughtException);
         }
 
         [TestMethod]
 
         public void ApartmentRatingIsEmpty()
         {
-            
+            Exception caughtException = null;
+            try
+            {
+                TestApartment.ApartmentId = "";
+            }
+            catch (Exception exceptionThrown)
+            {
+                caughtException = exceptionThrown;
+            }
+            Assert.IsNotNull(caughtException);
         }
 
         [TestMethod]
         public void IsApartmentAvailableIsNull()
         {
-            
+            Exception caughtException = null;
+            try
+            {
+                TestApartment.ApartmentId = null;
+            }
+            catch (Exception exceptionThrown)
+            {
+                caughtException = exceptionThrown;
+            }
+            Assert.IsNotNull(caughtException);
         }
 
         [TestMethod]
         public void IsApartmentAvailableIsEmpty()
         {
-            
+            Exception caughtException = null;
+            try
+            {
+                TestApartment.ApartmentId = "";
+            }
+            catch (Exception exceptionThrown)
+            {
+                caughtException = exceptionThrown;
+            }
+            Assert.IsNotNull(caughtException);
         }
 
         [TestMethod]
 
         public void ApartmentPriceByNightIsNull()
         {
-            
+            Exception caughtException = null;
+            try
+            {
+                TestApartment.ApartmentCoverImage = null;
+            }
+            catch (Exception exceptionThrown)
+            {
+                caughtException = exceptionThrown;
+            }
+            Assert.IsNotNull(caughtException);
         }
 
         [TestMethod]
         public void ApartmentPriceByNightIsEmpty()
         {
-            
+            Exception caughtException = null;
+            try
+            {
+                TestApartment.ApartmentId = "";
+            }
+            catch (Exception exceptionThrown)
+            {
+                caughtException = exceptionThrown;
+            }
+            Assert.IsNotNull(caughtException);
         }
 
         [TestMethod]
         public void NumberOfNightSpentIsNull()
         {
-            
+            Exception caughtException = null;
+            try
+            {
+                TestApartment.ApartmentCoverImage = null;
+            }
+            catch (Exception exceptionThrown)
+            {
+                caughtException = exceptionThrown;
+            }
+            Assert.IsNotNull(caughtException);
         }
 
         [TestMethod]
         public void NumberOfNightSpentIsEmpty()
         {
-            
+            Exception caughtException = null;
+            try
+            {
+                TestApartment.ApartmentId = "";
+            }
+            catch (Exception exceptionThrown)
+            {
+                caughtException = exceptionThrown;
+            }
+            Assert.IsNotNull(caughtException);
         }
 
         [TestMethod]
         public void ApartmentPriceTotalIsNull()
         {
-            
+            Exception caughtException = null;
+            try
+            {
+                TestApartment.ApartmentCoverImage = null;
+            }
+            catch (Exception exceptionThrown)
+            {
+                caughtException = exceptionThrown;
+            }
+            Assert.IsNotNull(caughtException);
         }
 
         [TestMethod]
         public void ApartmentPriceTotalIsEmpty()
         {
-            
+            Exception caughtException = null;
+            try
+            {
+                TestApartment.ApartmentId = "";
+            }
+            catch (Exception exceptionThrown)
+            {
+                caughtException = exceptionThrown;
+            }
+            Assert.IsNotNull(caughtException);
         }
     }
 }
